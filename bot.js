64fbfcbc1350ada76032fc8bb03843a1d0a71fd0
ZIKOpl <mathisalexandre89@gmail.com>
2025-09-15 23:22:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,10 +59,10 @@ function savePrices(p){ fs.writeFileSync(PRICES_FILE, JSON.stringify(p,null,2),"
 
 // --- Product meta ---
 const PRODUCTS = {
-  nitro1m: { name: "Nitro 1 mois", img: `${NETLIFY_ORIGIN}/Assets/nitro1.png` },
-  nitro1y: { name: "Nitro 1 an", img: `${NETLIFY_ORIGIN}/Assets/nitro2.png` },
-  boost1m: { name: "Nitro Boost 1 mois", img: `${NETLIFY_ORIGIN}/Assets/nitro3.png` },
-  boost1y: { name: "Nitro Boost 1 an", img: `${NETLIFY_ORIGIN}/Assets/nitro4.png` }
+  nitro1m: { name: "Nitro 1 mois", img1: `${NETLIFY_ORIGIN}/Assets/nitro1.png` },
+  nitro1y: { name: "Nitro 1 an", img2: `${NETLIFY_ORIGIN}/Assets/nitro2.png` },
+  boost1m: { name: "Nitro Boost 1 mois", img3: `${NETLIFY_ORIGIN}/Assets/nitro3.png` },
+  boost1y: { name: "Nitro Boost 1 an", img4: `${NETLIFY_ORIGIN}/Assets/nitro4.png` }
 };
 
 // --- Express ---
@@ -145,7 +145,7 @@ async function updateStockEmbed() {
         value: `💰 ${prices[key] ?? "N/A"}€\n📦 ${stock[key] ?? 0}`,
         inline: true
       });
-      if (!embed.data.thumbnail) embed.setThumbnail(p.img);
+      if (!embed.data.thumbnail) embed.setThumbnail(p.img4);
     }
 
     // Récupère ou envoie le message
@@ -310,3 +310,4 @@ client.once("ready", async () => {
 // Serveur Express
 app.listen(PORT, ()=> console.log(`API en ligne sur port ${PORT}`));
 client.login(DISCORD_TOKEN).catch(err => { console.error("Erreur login Discord:", err); process.exit(1); });
+
